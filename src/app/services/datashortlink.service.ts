2414c9bf942414864e7684bc749548d0844870de
albertomanuel.cahuiche <albertomanuel.cahuiche@gmail.com>
2024-05-20 20:07:22 +0000

    Se crean los metodos paara consulta y guardado de shortlinks

--- src/app/services/datashortlink.service.ts
+++ src/app/services/datashortlink.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
 
 // Suggested code may be subject to a license. Learn more: ~LicenseLog:3106212140.
 @Injectable({
@@ -10,12 +11,15 @@ export class DataShortlinkService{
     constructor(private httpClient: HttpClient){}
 
     save(url: string): Observable<any>{
-        const backurl = 'testurl'; // Pendiente agregar la URL del back end
-
+        
         const body = {
             "urlToSave": url
         }
 
-        return this.httpClient.post(backurl, body);
+        return this.httpClient.post(environment.backurl, body);
+    }
+
+    getUrl(key: string): Observable<any>{
+        return this.httpClient.get(environment.backurl + key)
     }
   }
