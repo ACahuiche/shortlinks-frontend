f31564c3a2264795ae6d95eb6e690504f1d4e1fb
albertomanuel.cahuiche <albertomanuel.cahuiche@gmail.com>
2024-05-21 21:03:00 +0000

    Se codifica el componente redirect y se elmina el componente main view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainViewComponent } from './main-view/main-view.component';
 import { ShortlinkFormComponent } from './shortlink-form/shortlink-form.component';
 import { RedirectComponent } from './redirect/redirect.component';
 import { NotFoundComponent } from './not-found/not-found.component';
@@ -16,7 +15,6 @@ import { DataShortlinkService } from './services/datashortlink.service';
 @NgModule({
   declarations: [
     AppComponent,
-    MainViewComponent,
     ShortlinkFormComponent,
     RedirectComponent,
     NotFoundComponent,
